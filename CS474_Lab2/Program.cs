15296d039196ce3053e1a231fbddf0bbbc6687bd
Shayna Conner <sconner15@wou.edu>
2019-05-09 23:00:29 -0700

    Start lab 2

---
+++ CS474_Lab2/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CS474_Lab2
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var sizes = new List<long>();
+            sizes.Add(1000);
+            sizes.Add(1000000);
+            sizes.Add(2000000);
+
+            foreach (var size in sizes)
+            {
+                SequentialSieve(size);
+                ParallelSieve(size);
+                Console.WriteLine();
+            }
+
+
+            Console.ReadLine();
+        }
+
+        private static void SequentialSieve(long size)
+        {
+            var primeArray = new bool[size];
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            // Set all to true
+            for (int i = 2; i < size; i++) primeArray[i] = true;
+
+            for (int i = 2; i < size; i++)
+            {
+                // If index is prime
+                if (primeArray[i])
+                {
+                    // Knock out multiples of index
+                    for (int j = i * 2; j < size; j += i)
+                    {
+                        primeArray[j] = false;
+                    }
+                }
+            }
+
+            // Count number of primes
+            var primeCount = 0;
+            foreach (var isPrime in primeArray)
+            {
+                if (isPrime) primeCount++;
+            }
+
+            stopwatch.Stop();
+            var elapsedTime = stopwatch.ElapsedMilliseconds;
+            stopwatch.Reset();
+
+            Console.WriteLine("Sequential: Found {0} prime numbers for array size {1}. Time: {2}ms", primeCount, size, elapsedTime);
+        }
+
+        private static void ParallelSieve(long size)
+        {
+            var primeArray = new bool[size];
+
+            // Get max 
+            var limit = (int) Math.Ceiling(Math.Sqrt(size));
+
+            // Set all except 0 and 1 to true
+            Parallel.For(2, primeArray.Length, i =>
+            {
+                if (i == 2 || i % 2 != 0) primeArray[i] = true;
+            });
+
+            Mutex mLock = new Mutex();
+
+            long elapsedTime = 0;
+            var options = new ParallelOptions{MaxDegreeOfParallelism = 4};
+
+            // Sequential loop that tracks latest prime
+            for (int latestPrime = 3; latestPrime < limit; latestPrime++)
+            {
+   
+                if (primeArray[latestPrime])
+                {
+                    var stopwatch = new Stopwatch();
+                    stopwatch.Start();
+                    // Question 4: Loop needs to go from 2 to ceiling of square root of size
+                    Parallel.For(3, limit, options, j =>
+                    {
+                        mLock.WaitOne();
+                        // Knock out multiples of prime i
+                        for (int k = latestPrime * 2; k < size; k += latestPrime)
+                        {
+                            primeArray[k] = false;
+                        }
+                        mLock.ReleaseMutex();
+                    });
+                    stopwatch.Stop();
+                    elapsedTime += stopwatch.ElapsedMilliseconds;
+
+                    stopwatch.Reset();
+                }
+
+            }
+
+            //Count
+            var primeCount = 0;
+
+            Parallel.For(0, primeArray.Length, i =>
+            {
+                mLock.WaitOne();
+                if (primeArray[i]) primeCount++;
+                mLock.ReleaseMutex();
+            });
+
+
+            Console.WriteLine("Parallel: Found {0} prime numbers for array size {1}. Time: {2}ms", primeCount, size, elapsedTime);
+        }
+    }
+}
